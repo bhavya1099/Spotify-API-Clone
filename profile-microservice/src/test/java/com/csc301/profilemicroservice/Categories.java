78bca05ba51770902506778d50eb2b7d7b207031
bhavya1099 <kinshu.1019@gmail.com>
2025-08-06 10:32:19 +0000

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ profile-microservice/src/test/java/com/csc301/profilemicroservice/Categories.java
@@ -0,0 +1,61 @@
+package com.csc301.profilemicroservice;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface InitProfileDb {
+
+	}
+
+	interface createUserProfile {
+
+	}
+
+	interface followFriend {
+
+	}
+
+	interface unfollowFriend {
+
+	}
+
+	interface getAllSongFriendsLike {
+
+	}
+
+	interface addProfile {
+
+	}
+
+	interface getAllFriendFavouriteSongTitles {
+
+	}
+
+	interface likeSong {
+
+	}
+
+	interface unlikeSong {
+
+	}
+
+	interface deleteAllSongsFromDb {
+
+	}
+
+}
